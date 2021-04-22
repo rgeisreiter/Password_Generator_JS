7869611f70a7e0f9c3a8011c700ad0db02636929
rgeisreiter <rgeisreiter@yahoo.com>
2021-04-22 10:07:30 -0600

    uploading correct template

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
-function generatePassword() {
-  var length = 22,
-    symbols = "dhbuy77rvnjhbJB7TTB5D65ryv0978bgb56dxHJBVHD4fdgS",
-    password = "";
-  for (var i = 0, i = symbols.length; i < length; ++i) {
-    password += symbols.charAt(Math.floor(Math.random() * n));
-    return password;
-  }
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
-console.log(generatePassword);
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
